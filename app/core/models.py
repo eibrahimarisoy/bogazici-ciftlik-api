2eb30e8df659bc3d1ca9d7112d96980e765974f9
eibrahimarisoy <eibrahimarisoy@gmail.com>
2020-07-24 10:25:09 +0300

    extended user model

--- app/core/models.py
+++ app/core/models.py
@@ -15,15 +15,41 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        """Creates and saves new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+    class Meta:
+        verbose_name = 'Kullanıcı'
+        verbose_name_plural = 'Kullanıcılar'
+
+    def get_full_name(self):
+        """Return the first_name plus last_name, with a space in between"""
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name
+
+    def get_short_name(self):
+        """Return the first_name plus last_name, with a space in between"""
+        return self.first_name
+
+    
