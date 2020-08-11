19064c3fc21588bc09c9312fc708eed6f2599f66
eibrahimarisoy <eibrahimarisoy@gmail.com>
2020-08-11 16:38:59 +0300

    added token based user authentication and create user views

---
+++ app/user/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import include, path
+
+from user import views
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
+]
--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import authentication, generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from .serializers import AuthTokenSerializer, UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create new user in the system"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permissions_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
